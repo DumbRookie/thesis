975afac97100fecef3bb554a2778b334ea813c22
Teo Flevaris <teo13flevaris@gmail.com>
2019-12-09 19:55:16 +0200

    Binominal SA Completed

--- analyzing tweets/not_relavant.py
+++
@@ -1,7 +0,0 @@
-from polyglot.text import Text
-
-text = Text(" Καλημέρα ", hint_language_code='el')
-
-print("{:<16}{}".format("Word", "Polarity")+"\n"+"-"*30)
-for w in text.words:
-    print (w, w.polarity)
